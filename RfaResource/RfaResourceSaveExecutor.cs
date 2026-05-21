b3fe62f853e6c82cb463594f8a347447f7a94d26
HuangYihang <1992831629@qq.com>
2026-05-21 10:11:59 +0800

    获取族的时候设置为详细视图

--- RfaResource/RfaResourceSaveExecutor.cs
+++ RfaResource/RfaResourceSaveExecutor.cs
@@ -35,6 +35,9 @@ namespace SiloModelingTaskClient
         /// <param name="log">日志输出方法</param>
         public void Execute(Document doc, View activeView, Action<string> log)
         {
+            SetActive3DViewDetailLevel(doc, activeView);
+            log("当前三维视图已设置为详细视图。");
+
             List<FamilyInstance> instances = _collector.CollectAllowedInstancesFromActive3DView(doc, activeView);
             string coordinateJsonPath = _coordinateExporter.Export(instances);
             log("族实例坐标文件已写入：" + coordinateJsonPath);
@@ -69,6 +72,26 @@ namespace SiloModelingTaskClient
 
                 _apiClient.AddRfaResource(record);
                 log("族资源已保存：" + family.FamilyName + "，文件地址：" + upload.FilePath);
+            }
+        }
+
+        /// <summary>
+        /// 将当前三维视图设置为详细视图
+        /// </summary>
+        /// <param name="doc">Revit文档</param>
+        /// <param name="activeView">当前视图</param>
+        private void SetActive3DViewDetailLevel(Document doc, View activeView)
+        {
+            if (!(activeView is View3D))
+            {
+                throw new InvalidOperationException("当前视图不是三维视图，不能设置详细视图。");
+            }
+
+            using (var transaction = new Transaction(doc, "设置三维视图详细程度"))
+            {
+                transaction.Start();
+                activeView.DetailLevel = ViewDetailLevel.Fine;
+                transaction.Commit();
             }
         }
     }
